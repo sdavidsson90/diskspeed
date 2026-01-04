5f29c0b8c8472b123daeedd8baeef45d53add76c
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2026-01-04 10:50:18 +0100

    Pushing this version

--- diskspeed.py
+++ diskspeed.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import shutil
+import statistics
 import time
 
 
@@ -20,6 +21,13 @@ class DiskSpeed:
             if str(size).endswith(unit):
                 return int(size[:-1]) * factor
 
+    @property
+    def identify_disk(self):
+        path = os.path.abspath(self.test_path)
+        while not os.path.ismount(path):
+            path = os.path.dirname(path)
+        return path
+
     def check_space(self):
         """
         Check if there's sufficient space to write the test_file in the specified size.
@@ -31,37 +39,104 @@ class DiskSpeed:
             raise RuntimeError("Not enough free space!")
 
     def test_diskspeed(self):
-        null_byte = b"\0"
         file_name = "diskspeed.tmp"
 
         self.check_space()
 
-        chunks = range(int(self.test_size / self.chunk_size))
+        chunks = int(self.test_size / self.chunk_size)
+
+        # file_content = b"\0" * self.chunk_size # Null bytes
+        file_content = os.urandom(self.chunk_size)  # Randomness
 
         try:
-            # --
-            print("Write speed:", end=" ")
+            print(f"Target: {self.identify_disk}", end="\n\n")
+
+            # -------------------------------------
+            gbps_measurements = []
+            mbps_measurements = []
+            print("Write speed:")
             with open(file_name, "wb") as f:
                 time_start = time.perf_counter()
-                for _ in chunks:
-                    f.write(null_byte * self.chunk_size)
+                for i in range(chunks):
+                    progress_pct = (i / chunks) * 100
+                    time_lap_start = time.perf_counter()
+                    f.write(file_content)
+                    time_lap_duration = time.perf_counter() - time_lap_start
+                    gbps = (self.test_size / self.UNITS["G"]) / time_lap_duration
+                    mbps = (self.test_size / self.UNITS["M"]) / time_lap_duration
+                    print(
+                        f"\r{progress_pct:.2f} %: {gbps:.3f} GB/s ({mbps:.0f} MB/s)",
+                        end="",
+                        flush=True,
+                    )
+                    gbps_measurements.append(gbps)
+                    mbps_measurements.append(mbps)
+                    f.seek(0)
 
-            time_duration = time.perf_counter() - time_start
-            gbps = (self.test_size / self.UNITS["G"]) / time_duration
-            mbps = (self.test_size / self.UNITS["M"]) / time_duration
-            print(f"{gbps:.3f} GB/s - ({mbps:.3f})")
+            gbps_mean = statistics.mean(gbps_measurements)
+            gbps_min = min(gbps_measurements)
+            gbps_max = max(gbps_measurements)
 
-            # --
-            print("read speed:", end=" ")
+            mbps_mean = statistics.mean(mbps_measurements)
+            mbps_min = min(mbps_measurements)
+            mbps_max = max(mbps_measurements)
+
+            print("\r", end = "", flush = True)
+            print(f"  mean: {gbps_mean:.3f} gb/s ({mbps_mean:.0f} mb/s)")
+            print(f"  min: {gbps_min:.3f} gb/s ({mbps_min:.0f} mb/s)")
+            print(f"  max: {gbps_max:.3f} gb/s ({mbps_max:.0f} mb/s)")
+            try:
+                gbps_stdev = statistics.stdev(gbps_measurements)
+                mbps_stdev = statistics.stdev(mbps_measurements)
+                print(
+                    f"  standard deviation: {gbps_stdev:.3f} gb/s ({mbps_stdev:.0f} mb/s)"
+                )
+            except:
+                pass
+
+            # -------------------------------------
+            print()
+            print("Read speed:")
             with open(file_name, "rb") as f:
                 time_start = time.perf_counter()
-                for _ in chunks:
+                for i in range(chunks):
+                    progress_pct = (i / chunks) * 100
+                    time_lap_start = time.perf_counter()
                     f.read()
-            time_duration = time.perf_counter() - time_start
-            gbps = (self.test_size / self.units["g"]) / time_duration
-            mbps = (self.test_size / self.units["m"]) / time_duration
-            print(f"{gbps:.3f} gb/s - ({mbps:.0f} mb/s)")
+                    time_lap_duration = time.perf_counter() - time_lap_start
+                    gbps = (self.test_size / self.UNITS["G"]) / time_lap_duration
+                    mbps = (self.test_size / self.UNITS["M"]) / time_lap_duration
+                    print(
+                        f"\r{progress_pct:.2f} %: {gbps:.3f} GB/s ({mbps:.0f} MB/s)",
+                        end="",
+                        flush=True,
+                    )
+                    gbps_measurements.append(gbps)
+                    mbps_measurements.append(mbps)
 
+            gbps_mean = statistics.mean(gbps_measurements)
+            gbps_min = min(gbps_measurements)
+            gbps_max = max(gbps_measurements)
+
+            mbps_mean = statistics.mean(mbps_measurements)
+            mbps_min = min(mbps_measurements)
+            mbps_max = max(mbps_measurements)
+
+            print("\r", end = "", flush = True)
+            print(f"  mean: {gbps_mean:.3f} gb/s ({mbps_mean:.0f} mb/s)")
+            print(f"  min: {gbps_min:.3f} gb/s ({mbps_min:.0f} mb/s)")
+            print(f"  max: {gbps_max:.3f} gb/s ({mbps_max:.0f} mb/s)")
+            try:
+                gbps_stdev = statistics.stdev(gbps_measurements)
+                mbps_stdev = statistics.stdev(mbps_measurements)
+                print(
+                    f"  standard deviation: {gbps_stdev:.3f} gb/s ({mbps_stdev:.0f} mb/s)"
+                )
+            except:
+                pass
+            # -------------------------------------
+
+            
         except KeyboardInterrupt:
             pass
 
@@ -78,7 +153,7 @@ def main():
         "--path",
         type=str,
         default=os.getcwd(),
-        help="Path to the measure target (disk)",
+        help="Path to the target disk to perform measurement on",
     )
     parser.add_argument(
         "-t",
@@ -93,6 +168,13 @@ def main():
         type=str,
         default="100M",
         help="Test file is written in chunks of 100M - use this option to override the default value",
+    )
+    parser.add_argument(
+        "-r",
+        "--repeat",
+        type=int,
+        default="100",
+        help="How many times to repeat the test",
     )
 
     args = parser.parse_args()
